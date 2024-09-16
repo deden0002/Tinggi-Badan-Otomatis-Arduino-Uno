43f28ab8943016eae55240f2f72b4eb32c90c9d6
Denia Aldi Saputra <deniaaldisaputra@gmail.com>
2024-09-17 00:20:54 +0700

    Add files via upload

---
+++ tinggi_badan.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#define trigPin 22
+#define echoPin 23
+LiquidCrystal_I2C lcd(0x27 ,16,2);
+int jarak = 10;
+int tinggi;
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  lcd.begin();
+    lcd.setCursor(2,0);
+    lcd.print("SISTEM AKTIF");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Tinggi : ");
+}
+void loop() {
+  long duration, gape;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  gape = (duration/2) / 29.0;
+  tinggi = jarak - gape;
+    lcd.setCursor(8,0);
+    lcd.print(tinggi);
+    lcd.print(" ");
+    lcd.print("CM");
+    lcd.print("   ");
+    delay(1500);
+}
